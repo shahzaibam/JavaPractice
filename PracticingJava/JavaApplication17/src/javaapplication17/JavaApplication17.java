7d8d764d8a237b8b1479710a316ef0341e21cf10
Shah <shebiraaja@gmail.com>
2022-10-23 17:34:56 +0200

    initial tr

--- PracticingJava/JavaApplication17/src/javaapplication17/JavaApplication17.java
+++ PracticingJava/JavaApplication17/src/javaapplication17/JavaApplication17.java
@@ -15,6 +15,8 @@ public class JavaApplication17 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("hey probando");
     }
     
 }
